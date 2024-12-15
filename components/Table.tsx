349012e014ba638b87e674ce561b3f8478fab125
bryan <bryanbayoca686@gmail.com>
2024-12-15 12:37:25 +0800

    typings

--- components/Table.tsx
+++ components/Table.tsx
@@ -244,7 +244,7 @@ export const users = [
     },
 ];
 
-export function capitalize(s) {
+export function capitalize(s: string): string {
     return s ? s.charAt(0).toUpperCase() + s.slice(1).toLowerCase() : "";
 }
 
