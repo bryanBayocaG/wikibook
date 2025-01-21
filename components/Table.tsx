ac7e787c22798035ba922654a63aabcf4c606731
bryanBayocaG <bryanbayoca@thelewiscollege.edu.ph>
2025-01-21 22:37:57 +0800

    table

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useMemo, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
     Table,
     TableHeader,
